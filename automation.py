5d41679b7c91c84db9967594f53abb6d7066082f
vrajasodariya07 <158601453+vrajasodariya07@users.noreply.github.com>
2024-10-04 18:36:39 +0530

    denny

--- automation.py
+++ automation.py
@@ -72,11 +72,11 @@ def get_chrome_options():
     # options.add_argument("--disable-gpu")
     # options.add_argument("--window-size=1920x1080")  # Set window size (some websites require this)
     # options.add_argument("--log-level=1")
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--window-size=1920,1080")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--window-size=1920,1080")
 
     # Set the binary location for Chromium (important for Render)
     # options.binary_location = "/usr/bin/chromium-browser"
